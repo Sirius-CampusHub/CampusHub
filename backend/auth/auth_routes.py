2406440e516b928e08d0bc22ee4f8e5dadfb918f
IvanSobolev <i@bezlikiy.ru>
2026-04-02 17:14:29 +0300

    fix: ошибка с фаербэйсом

--- backend/auth/auth_routes.py
+++ backend/auth/auth_routes.py
@@ -3,9 +3,6 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 import firebase_admin
 from firebase_admin import auth, credentials
 
-cred = credentials.Certificate("path/to/your/firebase-adminsdk.json")
-firebase_admin.initialize_app(cred)
-
 router = APIRouter(
     prefix="/auth",
     tags=["Auth"],
